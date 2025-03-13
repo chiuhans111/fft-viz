7941d783afb6de9971b0de93f279466cf0819661
Hans Chiu <chiuhans111@gmail.com>
2025-03-14 00:11:35 +0800

    fft

--- src/main.js
+++ src/main.js
@@ -43,54 +43,74 @@ function createFFTMatrix(radices) {
   }
 
 
-  
+  function _split(matrix, radices, x0, y0, j2dn, i2j2dn2) {
+    const n0 = matrix.length
+    const n2 = radices[0]
+    const n1 = Math.floor(n0 / n2)
+
+    if (radices.length == 0) {
+
+      for (let i = 0; i < n0; i += 1) {
+        for (let j = 0; j < n0; j += 1) {
+          matrix[i][j].x.target = j + x0
+          matrix[i][j].y.target = i + y0
+
+          matrix[i][j].time.target = i * j / n0 + j2dn * i + i2j2dn2
+        }
+      }
 
 
+      return n0+0.5
+    }
+
+    const matrix_groups = []
+
+    let y1 = y0
+
+    let size = 0
+
+    for (let i2 = 0; i2 < n2; i2++) {
+      let x1 = x0
+
+      for (let j2 = 0; j2 < n2; j2++) {
+
+
+        const s_matrix = []
+        for (let i1 = 0; i1 < n1; i1++) {
+          const row = []
+          for (let j1 = 0; j1 < n1; j1++) {
+            let i = i1 + i2 * n1
+            let j = j1 * n2 + j2
+            row.push(matrix[i][j])
+          }
+          s_matrix.push(row)
+        }
+
+        size = _split(s_matrix, radices.slice(1), x1, y1, j2/n0, i2*j2/n2)
+
+        x1 += size
+      }
+
+      y1 += size
+    }
+
+    return size * n2+0.5
+  }
 
 
 
 
   function split() {
-
-    let n1 = radices[0]
-    let n2 = radices[1]
-
-    for (let j = 0; j < N; j++) {
-      let j1 = Math.floor(j / n1)
-      let j2 = Math.floor(j % n1)
-
-      setTimeout(() => {
-        inputs[j].x.target = j1 + j2 * (n2 + 0.2)
-        inputs[j].y.acceleration += j2
-      }, j2 * 100);
-    }
-
-    for (let i = 0; i < N; i++) {
-
-      let i1 = Math.floor(i % n2)
-      let i2 = Math.floor(i / n2)
-
-      outputs[i].y.target = i1 + i2 * (n2 + 0.2)
-
-      for (let j = 0; j < N; j++) {
-        let j1 = Math.floor(j / n1)
-        let j2 = Math.floor(j % n1)
-        setTimeout(() => {
-
-
-          matrix[i][j].x.target = j1 + j2 * (n2 + 0.2)
-          matrix[i][j].y.target = i1 + i2 * (n2 + 0.2)
-          matrix[i][j].y.acceleration += j2
-        }, j2 * 100 + i * 50);
-
-      }
-    }
-
+    _split(matrix, radices.slice(0, 2), 0, 0)
   }
+
+
+
+
   return { split }
 }
 
-const fft = createFFTMatrix([3, 2])
+const fft = createFFTMatrix([2, 2, 2])
 
 function update(time) {
   requestAnimationFrame(update)
